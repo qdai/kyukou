b45a4ab0dfdf319e8ab05261bf3f9a95b81d11d5
egy186 <egy186@yahoo.co.jp>
2016-05-17 21:26:10 +0900

    remove appcache

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,12 @@
 'use strict';
 
-const config = require('config');
 const express = require('express');
 const path = require('path');
 
 const router = express.Router();
-const site = config.get('site');
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
-});
-
-router.get('/kyukou.appcache', (req, res) => {
-  res.set('Content-Type', 'text/cache-manifest; charset=UTF-8');
-  res.send(`CACHE MANIFEST
-# ${site.version}
-
-CACHE:
-/js/app.bundle.js
-
-NETWORK:
-*
-`);
 });
 
 module.exports = router;
