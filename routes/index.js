0f2d324e6cf146b9b410464b1053ebb7ff92d4cc
egy186 <egy186@yahoo.co.jp>
2016-09-26 23:27:13 +0900

    rename files in src

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+  res.sendFile(path.join(__dirname, '../views/app.html'));
 });
 
 module.exports = router;
