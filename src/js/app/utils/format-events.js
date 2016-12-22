18f429adad75ac098f96c16e386596d8501b708b
egy186 <egy186@yahoo.co.jp>
2016-12-22 17:16:46 +0900

    update calendar formatter

--- src/js/app/utils/format-events.js
+++ src/js/app/utils/format-events.js
@@ -1,14 +1,39 @@
 import 'moment/locale/ja';
 import moment from 'moment';
 
+moment.calendarFormat = (myMoment, now) => {
+  const daysDiff = myMoment.diff(now, 'days', true);
+  const weeksDiff = myMoment.diff(now.clone().startOf('week'), 'weeks', true);
+  if (daysDiff < -1) {
+    if (weeksDiff < -1) {
+      return 'sameElse';
+    } else if (weeksDiff < 0) {
+      return 'lastWeek';
+    }
+    return 'thisWeek';
+  } else if (daysDiff < 0) {
+    return 'lastDay';
+  } else if (daysDiff < 1) {
+    return 'sameDay';
+  } else if (daysDiff < 2) {
+    return 'nextDay';
+  } else if (weeksDiff < 1) {
+    return 'thisWeek';
+  } else if (weeksDiff < 2) {
+    return 'nextWeek';
+  }
+  return 'sameElse';
+};
+
 moment.updateLocale('ja', {
   calendar: {
     lastDay: '[昨日]',
-    lastWeek: 'YYYY年M月D日（dd）',
+    lastWeek: '先週dddd',
     nextDay: '[明日]',
-    nextWeek: 'dddd',
+    nextWeek: '来週dddd',
     sameDay: '[今日]',
-    sameElse: 'YYYY年M月D日（dd）'
+    sameElse: 'M月D日（dd）',
+    thisWeek: '今週dddd'
   }
 });
 
