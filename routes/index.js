46b4e6409080ec81e5bfc37562070a541ff49694
egy186 <egy186@yahoo.co.jp>
2017-09-22 00:45:08 +0900

    add more icons (#103)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,43 @@
 'use strict';
 
+const config = require('config');
 const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+const site = config.get('site');
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/app.html'));
+});
+
+router.get('/manifest.webmanifest', (req, res) => {
+  res.type('application/manifest+json');
+  res.json({
+    background_color: '#ffffff',
+    display: 'standalone',
+    icons: [
+      {
+        sizes: '144x144',
+        src: '/android-chrome-144x144.png',
+        type: 'image/png'
+      },
+      {
+        sizes: '192x192',
+        src: '/android-chrome-192x192.png',
+        type: 'image/png'
+      },
+      {
+        sizes: '512x512',
+        src: '/android-chrome-512x512.png',
+        type: 'image/png'
+      }
+    ],
+    name: site.name,
+    short_name: site.short_name,
+    start_url: '/',
+    theme_color: site.theme_color
+  });
 });
 
 module.exports = router;
