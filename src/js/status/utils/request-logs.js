efd0e2fdf46f6bf63a8cc93b8c1b3534db230c7f
egy186 <egy186@yahoo.co.jp>
2017-10-10 21:53:43 +0900

    check response status

--- src/js/status/utils/request-logs.js
+++ src/js/status/utils/request-logs.js
@@ -4,7 +4,12 @@ import fetch from 'isomorphic-fetch';
 const requestLogs = () => {
   const fetchLog = logName =>
     fetch(`${siteUrl}/api/1/logs/${logName}.json`)
-      .then(res => res.json());
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.json().then(result => Promise.reject(new Error(result.error.message)));
+      });
   return Promise.all(logNames.map(fetchLog));
 };
 
--- src/js/utils/request-events.js
+++ src/js/utils/request-events.js
@@ -3,6 +3,11 @@ import { siteUrl } from './constant';
 
 const requestEvents = () =>
   fetch(`${siteUrl}/api/1/events/list.json`)
-    .then(res => res.json());
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return res.json().then(result => Promise.reject(new Error(result.error.message)));
+    });
 
 export default requestEvents;
