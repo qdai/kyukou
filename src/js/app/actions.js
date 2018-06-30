18ff8acaa9c768e9df6bfeba9a3fd7f758c6f98e
egy186 <egy186@yahoo.co.jp>
2018-06-30 18:34:31 +0900

    fix errors

--- src/js/app/actions.js
+++ src/js/app/actions.js
@@ -1,8 +1,16 @@
 import { LOAD_EVENTS_FAILURE, LOAD_EVENTS_REQUEST, LOAD_EVENTS_SUCCESS, TOGGLE_ABOUT, TOGGLE_DEPARTMENT } from './action-types';
 import { createAction } from 'redux-actions';
 
-export const loadEventsRequest = createAction(LOAD_EVENTS_REQUEST);
-export const loadEventsFailure = createAction(LOAD_EVENTS_FAILURE);
-export const loadEventsSuccess = createAction(LOAD_EVENTS_SUCCESS);
-export const toggleAbout = createAction(TOGGLE_ABOUT);
-export const toggleDepartment = createAction(TOGGLE_DEPARTMENT);
+const loadEventsRequest = createAction(LOAD_EVENTS_REQUEST);
+const loadEventsFailure = createAction(LOAD_EVENTS_FAILURE);
+const loadEventsSuccess = createAction(LOAD_EVENTS_SUCCESS);
+const toggleAbout = createAction(TOGGLE_ABOUT);
+const toggleDepartment = createAction(TOGGLE_DEPARTMENT);
+
+export {
+  loadEventsRequest,
+  loadEventsFailure,
+  loadEventsSuccess,
+  toggleAbout,
+  toggleDepartment
+};
