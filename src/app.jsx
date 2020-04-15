34b09f5f50c37146c9cd14d8649dbb21e16c75af
egy186 <egy186@yahoo.co.jp>
2020-04-15 19:58:17 +0900

    use workbox-webpack-plugin

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,9 @@ import ReactDOM from 'react-dom';
 ReactDOM.render(<App admin={false} />, document.querySelector('#app'));
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js').catch(err => {
-    console.error(err); // eslint-disable-line no-console
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').catch(err => {
+      console.error(err); // eslint-disable-line no-console
+    });
   });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin'); // eslint-disable-line no
 const env = require('./env');
 const path = require('path');
 const webpack = require('webpack'); // eslint-disable-line node/no-unpublished-require
+const { GenerateSW } = require('workbox-webpack-plugin'); // eslint-disable-line node/no-unpublished-require
 
 const dest = path.join(__dirname, 'public');
 const src = path.join(__dirname, 'src');
@@ -11,8 +12,7 @@ const src = path.join(__dirname, 'src');
 module.exports = {
   entry: {
     admin: path.join(src, 'admin.jsx'),
-    app: path.join(src, 'app.jsx'),
-    'service-worker': path.join(src, 'service-worker.js')
+    app: path.join(src, 'app.jsx')
   },
   mode: 'production',
   module: {
@@ -29,6 +29,6 @@ module.exports = {
     path: dest,
     publicPath: '/'
   },
-  plugins: [new webpack.DefinePlugin({ SITE: JSON.stringify(env.SITE) })],
+  plugins: [new webpack.DefinePlugin({ SITE: JSON.stringify(env.SITE) }), new GenerateSW({ exclude: ['admin'] })],
   resolve: { extensions: ['.js', '.jsx'] }
 };
